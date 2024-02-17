918e0c969cecf0e246e1ceece4fc09dcf8eda01b
vanoraco <linkmarsto@gmail.com>
2024-02-17 15:10:11 +0000

    refactored the forms into their own components

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -38,6 +38,7 @@
                 >Login</a
               >
             </li>
+
             <li class="flex-auto text-center">
               <a
                 class="block rounded py-3 px-4 transition"
@@ -51,144 +52,8 @@
               >
             </li>
           </ul>
-
-          <!-- Login Form -->
-          <vee-form v-show="tab === 'login'" @submit="login" :validation-schema="loginSchema">
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <vee-field
-                type="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-                name="email"
-              />
-              <ErrorMessage class="text-red-600" name="email" />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <vee-field
-                type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-                name="password"
-              />
-              <ErrorMessage class="text-red-600" name="password" />
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-            >
-              Submit
-            </button>
-          </vee-form>
-          <div
-            class="text-white text-center font-bold p-4 rounded mb-4"
-            v-if="reg_show_alert"
-            :class="reg_alert_variant"
-          >
-            {{ reg_alert_message }}
-          </div>
-          <!-- Registration Form -->
-          <vee-form
-            v-show="tab === 'register'"
-            :validation-schema="schema"
-            @submit="register"
-            :initial-values="userData"
-          >
-            <!-- Name -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Name</label>
-              <vee-field
-                type="text"
-                name="name"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name"
-              />
-              <ErrorMessage class="text-red-600" name="name" />
-            </div>
-            <!-- Email -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Email</label>
-              <vee-field
-                type="email"
-                name="email"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email"
-              />
-              <ErrorMessage class="text-red-600" name="email" />
-            </div>
-            <!-- Age -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Age</label>
-              <vee-field
-                type="number"
-                name="age"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              />
-              <ErrorMessage class="text-red-600" name="age" />
-            </div>
-            <!-- Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Password</label>
-              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
-                <input
-                  type="password"
-                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                  placeholder="Password"
-                  v-bind="field"
-                />
-                <div class="text-red-600" v-for="err in errors" :key="err">
-                  {{ err }}
-                </div>
-              </vee-field>
-            </div>
-            <!-- Confirm Password -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Confirm Password</label>
-              <vee-field
-                type="password"
-                name="confirm_password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password"
-              />
-              <ErrorMessage class="text-red-600" name="confirm_password" />
-            </div>
-            <!-- Country -->
-            <div class="mb-3">
-              <label class="inline-block mb-2">Country</label>
-              <vee-field
-                as="select"
-                name="country"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-              >
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
-                <option value="Antarctica">Antarctica</option>
-              </vee-field>
-              <ErrorMessage class="text-red-600" name="country" />
-            </div>
-            <!-- TOS -->
-            <div class="mb-3 pl-6">
-              <vee-field
-                name="tos"
-                value="1"
-                type="checkbox"
-                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
-              />
-              <label class="inline-block">Accept terms of service</label>
-              <ErrorMessage class="text-red-600 block" name="tos" />
-            </div>
-            <button
-              type="submit"
-              class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
-              :disabled="reg_in_submission"
-            >
-              Submit
-            </button>
-          </vee-form>
+          <login-form v-if="tab === 'login'" />
+          <register-form v-else />
         </div>
       </div>
     </div>
@@ -198,32 +63,14 @@
 <script>
 import { mapState, mapWritableState } from 'pinia'
 import useModalStore from '../stores/modal'
-import { ErrorMessage } from 'vee-validate'
+import LoginForm from './LoginForm.vue'
+import RegisterForm from './RegisterForm.vue'
+
 export default {
   name: 'AppAuth',
   data() {
     return {
-      tab: 'login',
-      schema: {
-        name: 'required|min:3|max:100|alpha_spaces',
-        email: 'required|min:3|max:100|email',
-        age: 'required|min_value:18|max_value:100',
-        password: 'required|min:9|max:100|excluded:password',
-        confirm_password: 'passwords_mismatched:@password',
-        country: 'required|country_excluded:Antarctica',
-        tos: 'tos'
-      },
-      loginSchema: {
-        email: 'required|email',
-        password: 'required'
-      },
-      userData: {
-        country: 'USA'
-      },
-      reg_in_submission: false,
-      reg_show_alert: false,
-      reg_alert_variant: 'bg-blue-500',
-      reg_alert_message: 'Please Wait! Your Account is being created'
+      tab: 'login'
     }
   },
   computed: {
@@ -232,22 +79,7 @@ export default {
       modalVisbility: 'isOpen'
     })
   },
-  methods: {
-    register(value) {
-      this.reg_show_alert = true
-      this.reg_in_submission = true
-      this.reg_alert_variant = 'bg-blue-500'
-      this.reg_alert_message = 'Please Wait! Your Account is being created'
-
-      this.reg_alert_variant = 'bg-green-500'
-      this.reg_alert_message = 'Success! Your Account is created'
-      console.log(value)
-    },
-    login(value) {
-      console.log(value)
-    }
-  },
-
-  components: { ErrorMessage }
+  methods: {},
+  components: { LoginForm, RegisterForm }
 }
 </script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class="text-white text-center font-bold mb-4 p-4"
+    v-if="login_show_alert"
+    :class="login_alert_variant"
+  >
+    {{ login_alert_message }}
+  </div>
+  <!-- Login Form -->
+  <vee-form @submit="login" :validation-schema="loginSchema">
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field
+        type="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email"
+        name="email"
+      />
+      <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field
+        type="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Password"
+        name="password"
+      />
+      <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button
+      type="submit"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+      :disabled="login_in_submission"
+    >
+      Submit
+    </button>
+  </vee-form>
+</template>
+
+<script>
+import { ErrorMessage } from 'vee-validate'
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      tab: 'login',
+      loginSchema: {
+        email: 'required|email',
+        password: 'required'
+      },
+      login_in_submission: false,
+      login_show_alert: false,
+      login_alert_variant: 'bg-blue-500',
+      login_alert_message: 'Please Wait! We are logging you'
+    }
+  },
+
+  methods: {
+    login(value) {
+      this.login_in_submission = true
+      this.login_show_alert = true
+      this.login_alert_variant = 'bg-blue-500'
+      this.login_alert_message = 'Please Wait! We are logging you'
+      this.login_alert_variant = 'bg-green-500'
+      this.login_alert_message = 'Success! You are now logged in'
+      console.log(value)
+    }
+  },
+  components: { ErrorMessage }
+}
+</script>
